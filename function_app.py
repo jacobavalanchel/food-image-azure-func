597d82409888ca31c25ff0f3e01e9b02cd3cf87b
Han Yi <hanyiwindows@live.com>
2024-02-16 16:31:55 +0800

    init

--- function_app.py
+++ function_app.py
@@ -1,11 +1,44 @@
 import azure.functions as func
 import logging
+import shutil
+from ultralytics import YOLO
+from azure.identity import DefaultAzureCredential
+from azure.storage.blob import BlobServiceClient,BlobClient,ContainerClient
+import os,uuid
 
-app = func.FunctionApp()
+def uploadToCt(data):
+    connection_string = "DefaultEndpointsProtocol=https;AccountName=imageclassds;AccountKey=ZfuhniUPWj/SsV/Nqod2lb80fvV0rg+NFl4NLOrz9TnjYLFfOEwhF/2qOOYBgEdhAyjk74ykNtLx+AStklYlPQ==;EndpointSuffix=core.windows.net"
+    app = func.FunctionApp()
+    try:
+        pic_dir="./runs/detect/predict/Five_tips_Stream.jpg"
+        with open(pic_dir, "rb") as file:
+            binary_data = file.read()
+        logging.info("try conneting to ct")
+        print("trial-logging")
 
-@app.blob_trigger(arg_name="myblob", path="food-image-task/{name}",
+        # Quickstart code goes here
+        blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+        blob_client = blob_service_client.get_blob_client(container="food-image-task", blob="result.jpg")
+        blob_client.upload_blob(binary_data,overwrite=True)
+        print("done uploading")
+        shutil.rmtree("./runs")
+        print("cleared")
+        logging.info("done uploading")
+    except Exception as ex:
+        logging.info('Exception:')
+        logging.info(ex)
+
+
+
+
+@app.blob_trigger(arg_name="myblob", path="imageclassds",
                                connection="imageclassds_STORAGE") 
-def food_image_blob_trigger(myblob: func.InputStream):
+def FoodRecogBlobTrigger(myblob: func.InputStream):
     logging.info(f"Python blob trigger function processed blob"
                 f"Name: {myblob.name}"
                 f"Blob Size: {myblob.length} bytes")
+    # 模型路径
+    model = YOLO("best.pt")
+
+    # 预测图片路径，输出结果保存
+    result = model(source='Five_tips_Stream.jpg', save=True)
--- predict.py
+++ predict.py
@@ -1,7 +1,31 @@
 from ultralytics import YOLO
-
+import logging
+import shutil
+from ultralytics import YOLO
+from azure.identity import DefaultAzureCredential
+from azure.storage.blob import BlobServiceClient,BlobClient,ContainerClient
 # 模型路径
-model = YOLO("F:\\jasonadvance\\Documents\\Tencent Files\\718769082\\FileRecv\\Predict\\best.pt")
+model = YOLO("best.pt")
 
 # 预测图片路径，输出结果保存
-result = model(source='F:\\jasonadvance\\Documents\\Tencent Files\\718769082\\FileRecv\\Predict\\Five_tips_Stream.jpg', save=True)
+result = model(source='Five_tips_Stream.jpg', save=True)
+
+connection_string = "DefaultEndpointsProtocol=https;AccountName=imageclassds;AccountKey=ZfuhniUPWj/SsV/Nqod2lb80fvV0rg+NFl4NLOrz9TnjYLFfOEwhF/2qOOYBgEdhAyjk74ykNtLx+AStklYlPQ==;EndpointSuffix=core.windows.net"
+
+try:
+    pic_dir="./runs/detect/predict/Five_tips_Stream.jpg"
+    with open(pic_dir, "rb") as file:
+        binary_data = file.read()
+    #data = open('./runs/detect/predict/Five_tips_Stream.jpg', 'r').read()
+    print("try conneting to ct")
+
+    # Quickstart code goes here
+    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+    blob_client = blob_service_client.get_blob_client(container="food-image-task", blob="result.jpg")
+    blob_client.upload_blob(binary_data,overwrite=True)
+    print("done uploading")
+    shutil.rmtree("./runs")
+    print("cleared")
+except Exception as ex:
+    print('Exception:')
+    print(ex)
